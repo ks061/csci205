8e6b121615d121d6c3791d5482b120207b2d2058
Kartikeya Sharma <ks061@bucknell.edu>
2018-08-22 12:14:28 -0400

    Lab01, Exercise 5, Complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2018
+ * Instructor: Professor Dancy
+ * Section: 12 PM
+ *
+ * Name: Kartikeya Sharma
+ * Date: 08/22/2018
+ *
+ * Lab / Assignment: lab01
+ *
+ * Description: A Hello class in Java.
+ *
+ * *****************************************/
+
+package lab01;
+
+public class Hello
+{
+    public static void main( String[] args ){
+        System.out.println("Programming is not a spectator sport!");
+	System.exit(0);
+    }
+}
