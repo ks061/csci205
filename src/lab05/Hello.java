c9bfd081048d5a1f39a33cfdc9bba4da1832982a
Kartikeya Sharma <ks061@bucknell.edu>
2018-09-04 18:28:26 -0400

    Lab05, Exercise 1, a simple hello world program.

---
+++ src/lab05/Hello.java
@@ -0,0 +1,39 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2018
+ *
+ * Name: Kartikeya Sharma
+ * Date: Sep 4, 2018
+ * Time: 12:46:49 PM
+ *
+ * Project: csci205
+ * Package: lab05
+ * File: Hello
+ * Description: This files contains a Hello class,
+               which is used as a programming tool
+               to become accustomed to the Java IDE
+               in Netbeans.
+ *
+ * ****************************************
+ */
+package lab05;
+
+/**
+ * The Hello class, which is a programming aid in becoming accustomed to the
+ * Java IDE in NetBeans.
+ *
+ * @author Kartikeya Sharma
+ * @version 0.1
+ */
+public class Hello {
+
+    /**
+     * Runs when file called with java command in command line.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Becoming a good programmer takes practice.");
+    }
+
+}
