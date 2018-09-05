2c1bb52d0d439eb2466d881ff84e845e7be34b3f
Kartikeya Sharma <ks061@bucknell.edu>
2018-09-04 20:54:14 -0400

    Lab05, Exercise 1, Refactored

--- src/lab05/Hello.java
+++ src/lab05/Hello.java
@@ -9,10 +9,10 @@
  * Project: csci205
  * Package: lab05
  * File: Hello
- * Description: This files contains a Hello class,
-               which is used as a programming tool
-               to become accustomed to the Java IDE
-               in Netbeans.
+ * Description: This file contains a Hello class,
+                which is used as a programming tool
+                to become accustomed to the Java IDE
+                in Netbeans.
  *
  * ****************************************
  */
@@ -31,26 +31,50 @@ import java.util.Scanner;
  */
 public class Hello {
 
+    /**
+     * Number of integers to generate.
+     */
     private static final int NUM_INTS = 10;
 
     /**
      * Greets the user and generates random numbers.
      *
      * @param args the command line arguments
+     * @see Arrays
      */
     public static void main(String[] args) {
-        System.out.println("Becoming a good programmer takes practice.");
-        System.out.println("Hello. What is your name? ");
-        Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.println(name + ", becoming a good programmer takes practice.");
+        greetUser();
 
+        int[] x = getArrayOfRandomNums();
+        System.out.println(Arrays.toString(x));
+    }
+
+    /**
+     * Generates an array of random integer values.
+     *
+     * @return Array of random integer values
+     * @see Random
+     */
+    private static int[] getArrayOfRandomNums() {
+        // Some simple code to test out the debugger
         Random rand = new Random();
         int[] x = new int[NUM_INTS];
         for (int i = 0; i <= 9; i++) {
             x[i] = rand.nextInt(100);
         }
-        System.out.println(Arrays.toString(x));
+        return x;
+    }
+
+    /**
+     * Greets user using name inputted when prompted.
+     *
+     * @see Scanner
+     */
+    private static void greetUser() {
+        System.out.println("Hello. What is your name? ");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.println(name + ", becoming a good programmer takes practice.");
     }
 
 }
