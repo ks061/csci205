0d98c454f268c7a3f25388044fd9aa8aa582cae1
Kartikeya Sharma <ks061@bucknell.edu>
2018-09-13 15:40:41 -0400

    Lab08, Exercise 1, Complete

---
+++ src/lab08/Coin.java
@@ -0,0 +1,26 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2018
+ *
+ * Name: Kartikeya Sharma
+ * Date: Sep 13, 2018
+ * Time: 1:23:14 PM
+ *
+ * Project: csci205
+ * Package: lab08
+ * File: Coin
+ * Description: This file contains enumerated
+                types of coins.
+ *
+ * ****************************************
+ */
+package lab08;
+
+/**
+ * Coin - A simple enumerated type to represent the different types of coins
+ *
+ * @author Kartikeya Sharma
+ */
+enum Coin {
+    NICKEL, DIME, QUARTER;
+}
